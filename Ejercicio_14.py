cedc243ac7a63f040d659a08f8a9bfad1ac1bade
Lauv27 <143473984+Lauv27@users.noreply.github.com>
2023-08-28 22:44:31 -0500

    Create Ejercicio_14.py

---
+++ Ejercicio_14.py
@@ -0,0 +1,8 @@
+# En la siguiente línea de Código se le pide al usuario ingresar un número.
+numero = float(input("Ingrese un número: "))
+# Con el número que se le pidió se hacen las siguientes operaciones.
+cuadrado = numero ** 2 # En la siguiente ecuación se está elevando al cuadrado el número que el usuario ingreso de esta forma se calcula el cuadrado.
+cubo = numero ** 3 # En la siguiente ecuación se está elevando al cubo el número que el usuario ingreso de esta forma se calcula el cubo.
+# Mostrar los resultados.
+print("El cuadrado de", numero, "es", cuadrado) # En esta línea de Código se muestra el resultado al cuadrado del número.
+print("El cubo de", numero, "es", cubo) # En esta línea de Código se muestra el resultado al cubo del número.
